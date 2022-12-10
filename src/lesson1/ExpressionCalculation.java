3efd16e2dabe1d0e1615e4e3f451807046b84779
shopi <shopin_va@mail.ru>
2022-12-10 17:19:39 +0700

    solved the tasks of the first lesson

---
+++ src/lesson1/ExpressionCalculation.java
@@ -0,0 +1,23 @@
+package lesson1;
+
+import java.util.Scanner;
+
+public class ExpressionCalculation {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Введите 4 числа: ");
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int num3 = scanner.nextInt();
+        int num4 = scanner.nextInt();
+        System.out.println(returnExpression(num1, num2, num3, num4));
+    }
+
+    private static double returnExpression(int a, int b, int c, int d) {
+        if (d == 0) {
+            return 0;
+        }
+        return a * (b + (c / d));
+    }
+}
---
+++ src/lesson1/Variables.java
@@ -0,0 +1,26 @@
+package lesson1;
+
+public class Variables {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println(a);
+
+        long b = 1_000_000L;
+        System.out.println(b);
+
+        float c = 3.5f;
+        System.out.println(c);
+
+        double d = 1.5;
+        System.out.println(d);
+
+        char e = 'i';
+        System.out.println(e);
+
+        boolean f = true;
+        System.out.println(f);
+
+        String str = "Hello";
+        System.out.println(str);
+    }
+}
